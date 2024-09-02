75e1828f297841577ad4f1ad9e768081e40ef660
Elli Cano <123842690+crazyelli@users.noreply.github.com>
2024-09-02 13:46:42 -0700

    added simple gui

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use iced::widget::{container, text};
+use iced::widget::{column, container, text, Column};
 use iced::{alignment, Element, Length, Sandbox, Settings};
 
 fn main() -> iced::Result {
     SampleBox::run(Settings::default())
 }
 
-struct SampleBox {}
+struct SampleBox {
+    samples: Vec<String>,
+}
 
 #[derive(Debug, Clone)]
 enum Message {}
@@ -14,7 +16,9 @@ impl Sandbox for SampleBox {
     type Message = Message;
 
     fn new() -> SampleBox {
-        Self {}
+        Self {
+            samples: vec!["Kick".to_owned(), "Clap".to_owned(), "Snare".to_owned()],
+        }
     }
 
     fn title(&self) -> String {
@@ -26,7 +30,7 @@ impl Sandbox for SampleBox {
     }
 
     fn view(&self) -> Element<'_, Self::Message> {
-        container(text("Hello World"))
+        container(samples_list_view(&self.samples))
             .height(Length::Fill)
             .width(Length::Fill)
             .align_x(alignment::Horizontal::Center)
@@ -37,4 +41,17 @@ impl Sandbox for SampleBox {
     fn theme(&self) -> iced::Theme {
         iced::Theme::CatppuccinMocha
     }
+}
+
+fn samples_list_view(samples: &Vec<String>) -> Element<'static, Message> {
+    let mut column = Column::new()
+        .spacing(20)
+        .align_items(iced::Alignment::Center)
+        .width(Length::Fill);
+
+    for value in samples {
+        column = column.push(text(value))
+    }
+
+    container(column).height(250.0).width(300).into()
 }
